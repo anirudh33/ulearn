b57e396c1f0d75492f8d52cbdf88ede9b666cfb8
ujjwalrawlley <ujjwal.rawlley@osscube.com>
2013-04-12 03:15:06 -0700

    indentation in admin module

--- views/AdminViews/AdminProfileView.php
+++ views/AdminViews/AdminProfileView.php
@@ -14,7 +14,6 @@ Sr.NO.  Version	  Updated by        Updated on          Description
 * ************************************************************************
 
 */
-
 if(isset($adminprofiledata[0]['profilepicture'])) {
 /*Creating profile picture image resource*/
 $image=imagecreatefromstring($adminprofiledata[0]['profilepicture']);
@@ -22,23 +21,13 @@ $image=imagecreatefromstring($adminprofiledata[0]['profilepicture']);
 imagejpeg($image,SITE_PATH."/assets/images/Views/profilepics/adminprofile".$_SESSION['userID'].".jpeg",20); 
 }
 $pageName="AdminProfileView";
-
-
 ?>
 	<header id="header1">
-	
 		<div class="wrapper">
-			
 			<span id="leftusernav"><?php echo $lang-> WELCOME;?>&nbsp<?php echo $adminprofiledata[0]['firstname'];?></span>
-			
 		</div>
-	
-		
 	</header>
 				<span id="usernav">  
-	
-
-	
 	<div id="content" class="clearfix">
 		<section id="left">
 			<div id="userStats" class="clearfix">
@@ -48,31 +37,21 @@ $pageName="AdminProfileView";
 				</div>
 				<?php if(!empty($adminprofiledata))
 				{?>
-					
 				<div class="data">
 					<h1><?php echo $adminprofiledata[0]['firstname'];?> &nbsp <?php echo $adminprofiledata[0]['lastname'];
 					?></h1>
 					<h3><?php echo $adminprofiledata[0]['qualification'];
 					?></h3>
-					
-					
-					
-					
-					
 				</div>
 				<?php }
 				else {?>
 						<div class="data">
 					<h1>Welcome</h1>
 					<h3>Administrator</h3>
-					
 					<h4><a href="http://ulearn.com/">http://ulearn.com/</a></h4>
-					
 					<div class="sep"></div>
-					
 				</div>	
 						<?php 	}?>
-				
 			</div><br><br>
 			<h1><?php echo $lang->ABOUTME; ?></h1>
 <br>
@@ -81,20 +60,15 @@ $pageName="AdminProfileView";
 		<h3><?php echo $lang->NAME; ?>:</h3> <h4> 
 		<?php echo $adminprofiledata[0]['firstname'];?>&nbsp<?php echo $adminprofiledata[0]['lastname'];?></h4>
 		<div class="line-separator"></div>	
-<h3><?php echo $lang->QUALIFICATION; ?>:  </h3> <h4><?php echo $adminprofiledata[0]['qualification'];?></h4>
-<div class="line-separator"></div>
-<h3><?php echo $lang->BIRTHDATE;?>:  </h3> <h4><?php echo $adminprofiledata[0]['dob'];?></h4>
-<div class="line-separator"></div>
-
-<h3><?php echo $lang->CONTACTNO;?>:  </h3> <h4><?php echo $adminprofiledata[0]['phone'];?></h4>
-<div class="line-separator"></div>
-<h3><?php echo $lang->ADDRESS;?>:  </h3> <h4><?php echo $adminprofiledata[0]['address'];?></h4>
-
+		<h3><?php echo $lang->QUALIFICATION; ?>:  </h3> <h4><?php echo $adminprofiledata[0]['qualification'];?></h4>
+		<div class="line-separator"></div>
+		<h3><?php echo $lang->BIRTHDATE;?>:  </h3> <h4><?php echo $adminprofiledata[0]['dob'];?></h4>
+		<div class="line-separator"></div>
+		<h3><?php echo $lang->CONTACTNO;?>:  </h3> <h4><?php echo $adminprofiledata[0]['phone'];?></h4>
+		<div class="line-separator"></div>
+	<h3><?php echo $lang->ADDRESS;?>:  </h3> <h4><?php echo $adminprofiledata[0]['address'];?></h4>
 	</div>
 		</div>
-		
 </section>
-
 	</div>
-
 </html>
